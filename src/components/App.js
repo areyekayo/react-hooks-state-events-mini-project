b0c220643c1d1fea5902ede0f1f01b7a34b5ea8e
Riko <riko.fluchel@gmail.com>
2025-03-22 13:31:48 -0400

    lift task state handling to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,19 +8,34 @@ import { CATEGORIES, TASKS } from "../data";
 // console.log({ CATEGORIES, TASKS });
 
 function App() {
-	//const [taskList, setTasks] = useState(TASKS);
+	const [taskList, setTasks] = useState(TASKS);
 	const [selectedCategory, setCategoryFilter] = useState("All");
 
 	function handleCategoryClick(event){
 		setCategoryFilter(event.target.value)
 	}
 
+	function handleDeleteTask(key){
+		const newTaskList = taskList.filter((task) => task.text !== key);
+		setTasks(newTaskList) 
+	}
+
+	const filteredTasks = taskList.filter((task) => {
+		if (selectedCategory === "All"){
+			return true
+		}
+		return task.category === selectedCategory
+	})
+
 	return (
 		<div className="App">
 		<h2>My tasks</h2>
 		<CategoryFilter categories={CATEGORIES} selectedCategory={selectedCategory} handleCategoryClick={handleCategoryClick}/>
 		<NewTaskForm />
-		<TaskList tasks={TASKS} selectedCategory={selectedCategory}/>
+		<TaskList 
+			tasks={filteredTasks} 
+			selectedCategory={selectedCategory}
+			handleDeleteTask={handleDeleteTask}/>
 		</div>
 	);
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,25 +1,11 @@
-import React, {useState} from "react";
+import React from "react";
 import Task from "./Task";
 
-function TaskList({tasks, selectedCategory}) {
-	const [taskList, setTasks] = useState(tasks);
-
-	function handleDeleteTask(key){
-		//console.log("delete key", key)
-		const newTaskList = taskList.filter((task) => task.text !== key);
-		setTasks(newTaskList) 
-	}
-	
-	const filteredTasks = taskList.filter((task) => {
-		if (selectedCategory === "All"){
-			return true
-		}
-		return task.category === selectedCategory
-	})
+function TaskList({tasks, handleDeleteTask}) {
 
   	return (
 		<div className="tasks">
-			{filteredTasks.map((task) => (
+			{tasks.map((task) => (
 				<Task key={task.text} text={task.text} category={task.category} handleDeleteTask={handleDeleteTask}/>
 			))}
 		</div>
